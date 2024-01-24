d81740e4a6befe37f996c9a908d8d653862ecb77
Udeorah Frank <franklynudeorah@gmail.com>
2024-01-24 17:01:25 +0100

    catch all undefined routes

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1', router)
 
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(404).send({ message: "Route not found" });
+});
+
 app.listen(PORT, ()=> {
     console.log(`server is running on port ${PORT}`);
 });
